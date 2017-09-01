3c440343148c27bd846b677ca4b0d026e482a272
tonirilix <jesusmontes.mx@hotmail.com>
2017-08-31 21:38:42 -0500

    Listen touch events
    Listen to touch events and transform the diferences between the first drag position and the last drag position to set a translate3d value

--- src/app/touch-drag-to-load/touch-drag-to-load.component.ts
+++ src/app/touch-drag-to-load/touch-drag-to-load.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/startWith';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/fromEvent';
 import { LoadNotificationService } from '../load-notification.service';
 
 
@@ -12,9 +16,19 @@ import { LoadNotificationService } from '../load-notification.service';
 })
 export class TouchDragToLoadComponent implements OnInit {
 
-  
+  touchstart$ = Observable.fromEvent<TouchEvent>(document, 'touchstart');
+  touchend$ = Observable.fromEvent<TouchEvent>(document, 'touchend');
+  touchmove$ = Observable.fromEvent<TouchEvent>(document, 'touchmove');
 
-  position$ = Observable.of(0);
+  drag$ = this.touchstart$
+    .switchMap((start)=> 
+      this.touchmove$
+        .map(move => move.touches[0].pageY - start.touches[0].pageY )
+        .takeUntil(this.touchend$)
+   );
+
+  position$ = this.drag$.startWith(0)
+
   positionTranslate3d$ = this.position$
     .map(p=> `translate3d(0, ${p - 70}px, 0)`)    
   rotateTransform$ = Observable.of(`rotate(0deg)`);
