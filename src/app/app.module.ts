fd692ce9e46f84490e606fab6a48f68b87af308f
tonirilix <jesusmontes.mx@hotmail.com>
2017-08-31 20:55:36 -0500

    Get data from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LatestNewsComponent } from './latest-news/latest-news.component';
+import { NewsFeedServiceService } from './news-feed-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LatestNewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [NewsFeedServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
